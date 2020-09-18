87e5b76beaffa99ef0e108cb3fdf58f43153b4e0
Danicon <dancon.alferes@gmail.com>
2020-09-18 13:55:26 -0300

    quarta aula

---
+++ Aula00/ex03.py
@@ -0,0 +1,3 @@
+nome = input('Qual é o seu nome? ')
+print('olá', nome,'!','Prazer em te conhecer!')
+print(f'Olá {nome}! Prazer em te conhecer!')
---
+++ Aula00/ex05.py
@@ -0,0 +1,4 @@
+n1 = float(input('Primeiro número: '))
+n2 = float(input('Segundo número: '))
+n3 = n1 + n2
+print(f'A soma é {n3}')
